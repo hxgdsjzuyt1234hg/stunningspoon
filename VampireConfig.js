41b58c9b2dacfe8d8bfd2231e41d9134c32339c2
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-07 21:40:24 +0530

    Add files via upload

---
+++ VampireConfig.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [6340221250], // ganti jadi id mu
+    botToken: "7810948381:AAH7DpbhDCEaBM7kOXcGd0GgDHKXmltDScE", //isi make token bot mu
+}
+
+module.exports = global;
