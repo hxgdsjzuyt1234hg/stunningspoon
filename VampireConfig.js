e40a30e6a157dfc40d4be304dc861e142db6b4f4
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-23 06:38:17 +0530

    Update VampireConfig.js

--- VampireConfig.js
+++ VampireConfig.js
@@ -1,6 +1,6 @@
 const global = {
     owner: [7886419837], // ganti jadi id mu
-    botToken: "8044604306:AAEuz52022yLlbAvoC7X4Vk2W7WY2hnFajk", //isi make token bot mu
+    botToken: "7585283331:AAFBTZMSJrBLhAvEYSTkS5OX93vFchNoHqw", //isi make token bot mu
 }
 
 module.exports = global;
